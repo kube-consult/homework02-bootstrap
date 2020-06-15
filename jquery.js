c5710e644cfa567436265eb40e743b33e8419973
Paul Foster <fostpau@Pauls-MacBook-Air.local>
2020-06-15 21:37:01 +1000

    init

---
+++ jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#dog1").append("<img src='Images/dog.jpeg' width=200 height=200/>");
+    $("#dog2").append("<img src='Images/dog2.jpeg' width=200 height=200/>");
+    $("#dragon").css({"background-image": "url(Images/dragon.jpeg)", "width": "100%", "height": "600","background-size": "cover"});
+    $("#space").css({"background-image": "url(Images/space.jpeg)", "width": "100%", "height": "600","background-size": "cover"}); 
+  });
